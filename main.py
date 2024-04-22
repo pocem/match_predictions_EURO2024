72779a947c0c903130a7d06992adb91a7e3f3a72
Michal Grega <155822212+pocem@users.noreply.github.com>
2024-04-22 15:14:20 +0200

    Adding security authentication with flask-login

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import mysql.connector
-from flask import Flask, request, session, jsonify
+from flask import Flask, request, session, jsonify, render_template
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from flask_cors import CORS
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import secrets
 
 
-
+secret_key = secrets.token_hex(16)
 
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="grepolismiso21", database="euro")
 
 myCursor = mydb.cursor()
 app = Flask(__name__)
+app.secret_key = secret_key
+login_manager = LoginManager(app)
 CORS(app)
 
 # SIGNUP--------------------------------------------------
@@ -36,24 +40,29 @@ def signup(name, password, age, supportingTeam):
 
 # LOGIN---------------------------------------------
 def login(name, password):
+    print(name)
+    try:
+        # Execute a SQL query to check login credentials
+        sql = "SELECT password_ FROM player WHERE name = %s"
+        myCursor.execute(sql, (name,))
+        result = myCursor.fetchone()
+        print(result)
 
-    # Execute a SQL query to check login credentials
-    sql = "SELECT password_ FROM player WHERE name = %s"
-    myCursor.execute(sql, (name,))
-    result = myCursor.fetchone()
-
-    # Check if a row was returned and if the provided password matches the stored password
-    if result:
-        if result[0] == password:
-            print("Login successful!")
-            return{"message": "Login successful"}
+        # Check if a row was returned and if the provided password matches the stored password
+        if result:
+            if result[0] == password:
+                print("Login successful!")
+                return {"message": "Login successful"}
+            else:
+                print("Incorrect password. Please try again.")
+                return {"message": "Incorrect password"}
         else:
-            print("Incorrect password. Please try again.")
-            return{"message" : "Incorrect password"}
+            print("Name not found. Please check your credentials or sign up.")
+            return {"message": "Name not found"}
+    except Exception as e:
+        print("An error occurred while querying the database:", e)
+        return {"message": "An error occurred while querying the database"}
 
-    else:
-        print("Name not found. Please check your credentials or sign up.")
-        return {"message": "Name not found"}
 
 
 # storing all info about the player in a dictionary
@@ -65,7 +74,24 @@ signup_data = myCursor.fetchall()
 for name, password, age, supporting_team in signup_data:
     player_credentials[name] = {'password': password, 'age': age, 'supporting_team': supporting_team}
 
+class User(UserMixin):
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
 
+    @staticmethod
+    def get(name):
+        # Retrieve user from the database based on name
+        # Return user object if found, otherwise return None
+        user_data_query = "SELECT password_ FROM player WHERE name = %s"
+        myCursor.execute(user_data_query, (name,))
+        user_password = myCursor.fetchone()
+        if user_password:
+            return User(name, user_password[0])  # Return User object with password
+        else:
+            return None
+    def get_id(self):
+        return self.name
 
 # Function to retrieve matches from the database
 def get_matches():
@@ -118,63 +144,63 @@ def top_charts():
     return top_players_data
 
 # check the predictions with real outcomes --- WEBSCRAPING/API
-while True:
-    try:
-        driver = webdriver.Chrome()
-        # Open the website
-        driver.get("https://www.flashscore.com/football/europe/euro/fixtures/")
-
-        # Find all div elements where the home scores are stored
-        score_home_elements = driver.find_elements(By.CLASS_NAME, "event__score--home")
-        # Find all div elements where the away scores are stored
-        score_away_elements = driver.find_elements(By.CLASS_NAME, "event__score--away")
-
-        # Initialize empty list to store match data
-        match_data = []
-
-        # Loop through the matches and extract the scores
-        for index, (score_home_element, score_away_element) in enumerate(zip(score_home_elements, score_away_elements), start=1):
-            score_home = score_home_element.text
-            score_away = score_away_element.text
-            match_data.append((index, score_home, score_away))
-
-        # Insert the scraped data into the database
-        insert_query = "INSERT INTO match_predictions (match_id, home_team_score, away_team_score) VALUES (%s, %s, %s)"
-        myCursor.executemany(insert_query, match_data)
-        mydb.commit()
-
-        # Output the success message
-        print("Scraped data inserted into database successfully.")
-
-        # Check if all matches have started
-        if all(score != "-" for _, score, _ in match_data):
-            # Evaluate points for the last match
-            matches = get_matches()
-            points = 0
-            for match_id, index in range(len(matches)):
-                match_id, home_prediction, away_prediction = player_predictions[match_id]
-                index, real_score_home, real_score_away = match_data[index]
-
-                if home_prediction == real_score_home and away_prediction == real_score_away:
-                    points += 4
-                elif home_prediction > away_prediction and real_score_home > real_score_away:
-                    points += 1
-                elif home_prediction < away_prediction and real_score_home < real_score_away:
-                    points += 1
-                elif home_prediction == away_prediction and real_score_home == real_score_away:
-                    points += 2
-            break  # Exit the loop if all matches have started
-
-    except Exception as e:
-        # Handle any exceptions gracefully
-        print("An error occurred:", str(e))
-
-    finally:
-        # Close the WebDriver to free up resources
-        driver.quit()
-
-    # Wait for 1 hour (3600 seconds) before the next update
-    time.sleep(3600)
+# while True:
+#     try:
+#         driver = webdriver.Chrome()
+#         # Open the website
+#         driver.get("https://www.flashscore.com/football/europe/euro/fixtures/")
+#
+#         # Find all div elements where the home scores are stored
+#         score_home_elements = driver.find_elements(By.CLASS_NAME, "event__score--home")
+#         # Find all div elements where the away scores are stored
+#         score_away_elements = driver.find_elements(By.CLASS_NAME, "event__score--away")
+#
+#         # Initialize empty list to store match data
+#         match_data = []
+#
+#         # Loop through the matches and extract the scores
+#         for index, (score_home_element, score_away_element) in enumerate(zip(score_home_elements, score_away_elements), start=1):
+#             score_home = score_home_element.text
+#             score_away = score_away_element.text
+#             match_data.append((index, score_home, score_away))
+#
+#         # Insert the scraped data into the database
+#         insert_query = "INSERT INTO match_predictions (match_id, home_team_score, away_team_score) VALUES (%s, %s, %s)"
+#         myCursor.executemany(insert_query, match_data)
+#         mydb.commit()
+#
+#         # Output the success message
+#         print("Scraped data inserted into database successfully.")
+#
+#         # Check if all matches have started
+#         if all(score != "-" for _, score, _ in match_data):
+#             # Evaluate points for the last match
+#             matches = get_matches()
+#             points = 0
+#             for match_id, index in range(len(matches)):
+#                 match_id, home_prediction, away_prediction = player_predictions[match_id]
+#                 index, real_score_home, real_score_away = match_data[index]
+#
+#                 if home_prediction == real_score_home and away_prediction == real_score_away:
+#                     points += 4
+#                 elif home_prediction > away_prediction and real_score_home > real_score_away:
+#                     points += 1
+#                 elif home_prediction < away_prediction and real_score_home < real_score_away:
+#                     points += 1
+#                 elif home_prediction == away_prediction and real_score_home == real_score_away:
+#                     points += 2
+#             break  # Exit the loop if all matches have started
+#
+#     except Exception as e:
+#         # Handle any exceptions gracefully
+#         print("An error occurred:", str(e))
+#
+#     finally:
+#         # Close the WebDriver to free up resources
+#         driver.quit()
+#
+#     # Wait for 1 hour (3600 seconds) before the next update
+#     time.sleep(3600)
 
 # showing the live match results--------------------------
 def match_results_shown():
@@ -201,6 +227,7 @@ def matches():
 def signup_web():
     data = request.get_json()  # Get JSON data from the request body
     # Access individual fields from the JSON data
+    print("Received login data:", data)
     name = data.get('name')
     password = data.get('password')
     age = data.get('age')
@@ -214,15 +241,41 @@ def signup_web():
 @app.route('/login', methods=['POST'])
 def login_web():
     data = request.get_json()  # Get JSON data from the request body
-    # Access individual fields from the JSON data
-    name = data.get('name')
+    print("Received login data:", data)  # Log received data for debugging
+    name = data.get('username')
     password = data.get('password')
 
-    message = login(name, password)
-    if message.get('message') == "Login successful":
-        session['player_name'] = name
+    user = User.get(name)  # Retrieve user from the database
 
-    return jsonify(message)
+    if user and user.password == password:
+        # Login successful, authenticate the user and start session
+        login_user(user)
+        return jsonify({"message": "Login successful"}), 200  # Send HTTP status code 200 (OK) for success
+    else:
+        return jsonify(
+            {"message": "Incorrect username or password"}), 401  # Send HTTP status code 401 (Unauthorized) for failure
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/protected')
+@login_required
+def protected():
+    return 'Protected view'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'Logged out successfully'
+
+
+
+
+
+
+
 
 # POST sending user's predictions to backend
 @app.route('/predictions', methods=['POST'])
