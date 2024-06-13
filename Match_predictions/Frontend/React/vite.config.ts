cef89725490b6625c9cc772fa718e16a5ff6e76f
pocem <misogrega21@gmail.com>
2024-06-13 22:55:11 +0200

    deploy

--- Match_predictions/Frontend/React/vite.config.ts
+++ Match_predictions/Frontend/React/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',  // Ensure this points to the directory containing index.html
+  root: './',  // Ensure this points to the directory containing index.html
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html'  // Ensure this is correct
+      input: './index.html'  // Specify the path to index.html relative to the root
     }
   }
-})
+});
