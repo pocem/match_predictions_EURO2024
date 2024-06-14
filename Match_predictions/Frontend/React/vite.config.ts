c5cd14b8afa60d858f5b169779efbb879d57c309
pocem <misogrega21@gmail.com>
2024-06-14 12:26:20 +0200

    deploy

--- Match_predictions/Frontend/React/vite.config.ts
+++ Match_predictions/Frontend/React/vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output directory for the built files
-    rollupOptions: {
-      input: {
-        main: './src/main.tsx' // Replace with your actual entry file path
-      }
-    }
-  }
-});
+})
